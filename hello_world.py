7a24d64fa919e642d32012346718e3909dc31c52
Kento.Yamada <ymd65536@outlook.com>
2025-07-02 11:38:11 +0000

    Add Hello World program in Python

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Simple Hello World program in Python
+"""
+
+def main():
+    """Main function that prints Hello World"""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
